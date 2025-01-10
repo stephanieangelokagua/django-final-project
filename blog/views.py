c33a3ab20fc7802bcd328e79a85ca7376e34a53b
Stephanie Okagua <okaguastephanieangel@gmail.com>
2025-01-10 12:32:38 +0000

    Add view function to test new app

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def my_blog(request):
+    return HttpRespondr("Hello Code Blog!") # test new app
