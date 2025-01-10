8d1025889f3e40e4a3ba47bb5bdb5dfd6d9a2b69
Stephanie Okagua <okaguastephanieangel@gmail.com>
2025-01-10 13:24:51 +0000

    install dj-database-url and psycopg2

--- code_blog/settings.py
+++ code_blog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-f$-zk&^!2272f(bcuc(8i2+hcgfbb$+1h&5+oh=8)fp--rhz*n'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-stephaniean-djangofinal-fwovunktxpk.ws.codeinstitute-ide.net', '.herokuapp.com']
 
