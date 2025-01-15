2eb5c9e52d4d94cf726078026b7dae2bf1f96a15
Stephanie Okagua <okaguastephanieangel@gmail.com>
2025-01-15 14:52:21 +0000

    add fixtures for posts

--- code_blog/urls.py
+++ code_blog/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 # from blog.views import my_blog
 
 urlpatterns = [
-    path("", include("blog.urls"), name="blog-urls"),
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
+    path("", include("blog.urls"), name="blog-urls"),
 ]
